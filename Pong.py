37300ee29e79a2faef7c14ba62d4920db06c25d3
Shivam Shekhar <shivam.shekhar.ece14@itbhu.ac.in>
2016-10-17 13:26:58 +0530

    added scoreboard, decreased flickering of cpu paddle

--- Pong.py
+++ Pong.py
@@ -16,9 +16,15 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode(scr_size)
 pygame.display.set_caption('Pong')
 
+def displaytext(text,fontsize,x,y,color):
+    font = pygame.font.SysFont('sawasdee', fontsize, True)
+    text = font.render(text, 1, color)
+    textpos = text.get_rect(centerx=x, centery=y)
+    screen.blit(text, textpos)
+
 def cpumove(cpu,ball):
     if ball.movement[0] > 0:
-        if ball.rect.top > cpu.rect.top:
+        if ball.rect.bottom > cpu.rect.bottom:
             cpu.movement[1] = 8
         elif ball.rect.top < cpu.rect.top:
             cpu.movement[1] = -8
@@ -41,6 +47,7 @@ class Paddle(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.left = self.x
         self.rect.top = self.y
+        self.points = 0
         self.movement = [0,0]
 
     def checkbounds(self):
@@ -76,6 +83,7 @@ class Ball(pygame.sprite.Sprite):
         self.rect.centerx = x
         self.rect.centery = y
         self.maxspeed = 10
+        self.score = 0
         self.movement = movement
 
     def checkbounds(self):
@@ -91,8 +99,17 @@ class Ball(pygame.sprite.Sprite):
     def update(self):
         if self.rect.top == 0 or self.rect.bottom == height:
             self.movement[1] = -1*self.movement[1]
-        if self.rect.left == 0 or self.rect.right == width:
-            self.movement[0] = -1*self.movement[0]
+        if self.rect.left == 0:
+            self.rect.centerx = width/2
+            self.rect.centery = height/2
+            self.movement = [random.randrange(-1,2,2)*4,random.randrange(-1,2,2)*4]
+            self.score = 1
+            #self.movement[0] = -1*self.movement[0]
+        if self.rect.right == width:
+            self.rect.centerx = width/2
+            self.rect.centery = height/2
+            self.movement = [random.randrange(-1,2,2)*4,random.randrange(-1,2,2)*4]
+            self.score = -1
 
         self.rect = self.rect.move(self.movement)
         self.checkbounds()
@@ -107,7 +124,7 @@ def main():
     gameOver = False
     paddle = Paddle(width/10,height/2,width/60,height/8,(255,255,255))
     cpu = Paddle(width - width/10,height/2,width/60,height/8,(255,255,255))
-    ball = Ball(width/2,height/2,15,(255,0,0),[4,4])
+    ball = Ball(width/2,height/2,12,(255,0,0),[4,4])
     while not gameOver:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -127,6 +144,9 @@ def main():
         cpu.draw()
         ball.draw()
 
+        displaytext(str(paddle.points),20,width/8,25,(255,255,255))
+        displaytext(str(cpu.points),20,width - width/8,25,(255,255,255))
+
         if pygame.sprite.collide_mask(paddle,ball):
             ball.movement[0] = -1*ball.movement[0]
             ball.movement[1] = ball.movement[1] - int(0.1*random.randrange(5,10)*paddle.movement[1])
@@ -143,6 +163,12 @@ def main():
             if ball.movement[1] < -1*ball.maxspeed:
                 ball.movement[1] = -1*ball.maxspeed
 
+        if ball.score == 1:
+            cpu.points += 1
+            ball.score = 0
+        elif ball.score == -1:
+            paddle.points += 1
+            ball.score = 0
 
         paddle.update()
         ball.update()
